d2fc2d39270bec8009113e3202333f4c53019245
Aaron Stone <aaron@serendipity.cx>
2012-10-18 16:39:43 -0700

    More rails 3 configs.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PuppetDashboard::Application.config.secret_token = 'c6f6e3e8fcd3abed2a888d0fd543ccb8a2628f2039e074fc7ac8aa5b8d4aeac8d0cfa567deee2126d24466f90733a66c6093d5e6b3e1a744099f4955fdfab62c'
